c541a703877107e087394b4611575541b51a5c2b
MuratMjdci <murat.mjdci@gmail.com>
2021-12-17 19:25:55 +0200

    update

--- lib/flutter_cblue.dart
+++ lib/flutter_cblue.dart
@@ -1,31 +1,30 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class FlutterCblue {
   static const MethodChannel _channel = const MethodChannel('flutter_cblue');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static Future<String> printToBT(
-      {@required String printStr, deviceName}) async {
-    final Map<String, dynamic> params = <String, dynamic>{
+  static Future<String?> printToBT({
+    required String printStr,
+    required String deviceName,
+  }) async {
+    final params = <String, dynamic>{
       'printStr': printStr,
       'deviceName': deviceName
     };
-    final String result = await _channel.invokeMethod('printToBt', params);
+    final String? result = await _channel.invokeMethod('printToBt', params);
     return result;
   }
 
-  static Future<bool> checkDevice({@required deviceName}) async {
-    final Map<String, dynamic> params = <String, dynamic>{
-      'deviceName': deviceName
-    };
-    final bool result = await _channel.invokeMethod('checkDevice', params);
+  static Future<bool?> checkDevice({required deviceName}) async {
+    final params = <String, dynamic>{'deviceName': deviceName};
+    final bool? result = await _channel.invokeMethod('checkDevice', params);
     return result;
   }
 }
